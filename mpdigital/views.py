f2f1df70be44c534ad5ce4160b1f3a939d6248a7
Melvin Payusan <melvin.a.payusan@gmail.com>
2022-11-10 20:19:25 +0800

    add nav

--- mpdigital/views.py
+++ mpdigital/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "mpdigital/index.html")
+
+
+def services(request):
+    return render(request, "mpdigital/services.html")
